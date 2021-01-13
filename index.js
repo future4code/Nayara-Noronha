d36fb559e3103d7feffe4e8bf42d0d2cf07b833a
Nayara <naay.lenzi@gmail.com>
2021-01-13 02:10:37 -0300

    exercícios aula 9

---
+++ index.js
@@ -0,0 +1,88 @@
+// EXERCÍICIOS DE INTERPRETAÇÃO DE CÓDIGOS //
+
+/* 1- É impresso no console o seguionte resultado:
+    
+    10
+
+    10 5
+
+    2- Será impresso no console:
+
+    10 
+    10 
+    10
+
+*/
+
+// EXERCÍCIOS DA ESCRITA DE CÓDIGO //
+
+    /* EXERCÍCIO 1
+
+    let nome
+    let idade
+
+    console.log (typeof nome, typeof idade)
+
+    //resultado: undefined undefined (nenhum valor definido)//
+
+    nome = prompt ('Qual seu nome?')
+    idade = prompt ('Qual a sua idade?')
+
+    console.log (typeof nome , typeof idade)
+
+    // o prompt sempre retorna uma string //
+
+    console.log ("olá ", nome , "você tem ", idade , "anos")
+    */
+
+/*
+    EXERCÍCIO 2
+
+    let nome = prompt("Qual seu nome?")
+    console.log(nome)
+
+    let animal = prompt("Você tem algum animal de estimação?")
+    console.log(animal)
+
+    let filme = prompt("Qual gênero de filme você mais gosta?")
+    console.log(filme)
+
+    let corPreferida = prompt("Sua cor preferida?")
+    console.log(corPreferida)
+
+    let comida = prompt("Prefere doce ou salgado?")
+    console.log(comida)
+
+    let pais = prompt("Qual país você quer conhecer?")
+    console.log(pais)
+
+*/
+
+/*  EXERCÍCIO 3  
+
+    let comidas = ["pizzas" , "árabe" , "feijoada" , "tortas" , "pão de queijo"]
+    console.log(comidas)
+
+    console.log("Essas são minhas comidas preferidas:")
+    console.log(comidas["0"])
+    console.log(comidas["1"])
+    console.log(comidas["2"])
+    console.log(comidas["3"])
+    console.log(comidas["4"])
+
+    let comidaUsuario = prompt("Qual a sua comida preferida?")
+    comidas["1"] =  comidaUsuario
+
+    console.log(comidas)
+
+*/
+
+/*  EXERCÍCIO 4
+
+    let perguntas = ["Você bebeu água hoje?" , "Você está com sono?" , "Você se alimentou?"]
+    let respostas = ["true" , "false" , "true"]
+    console.log(perguntas[0] , respostas[0])
+    console.log(perguntas[1] , respostas[1])
+    console.log(perguntas[2] , respostas[2])       // esse foi de quebrar a cabeça //
+
+*/
